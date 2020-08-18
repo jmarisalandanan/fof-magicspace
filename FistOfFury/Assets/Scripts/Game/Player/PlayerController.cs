e6e96df083c4c125084a3e8e31ca52eb38e02fbf
Jose Mari Salandanan <salandananjosemari@gmail.com>
2020-08-18 11:10:45 +0800

    Added test animations

--- FistOfFury/Assets/Scripts/Game/Player/PlayerController.cs
+++ FistOfFury/Assets/Scripts/Game/Player/PlayerController.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using Lean.Touch;
 
 namespace MagicSpace.LS
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField]
+        private Animation playerAnimator;
+
+        private List<string> attackAnimations = new List<string>();
         private Transform cachedTransform;
 
         public void Attack(SwipeDirection direction)
         {
             cachedTransform.eulerAngles = BattleUtils.GetVectorDirection(direction);
+            playerAnimator.Play(attackAnimations[Random.Range(0, attackAnimations.Count)]);
         }
 
         private void Awake()
         {
             cachedTransform = this.transform;
+            attackAnimations.Add("Attack_1");
+            attackAnimations.Add("Attack_2");
         }
     }
 }
