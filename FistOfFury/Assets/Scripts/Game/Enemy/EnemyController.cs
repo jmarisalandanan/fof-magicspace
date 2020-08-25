cb50c0cda203f64078566bdcc2dc7dae27127461
Jose Mari Salandanan <salandananjosemari@gmail.com>
2020-08-25 22:19:57 +0800

    New animations

--- FistOfFury/Assets/Scripts/Game/Enemy/EnemyController.cs
+++ FistOfFury/Assets/Scripts/Game/Enemy/EnemyController.cs
@@ -7,11 +7,9 @@ namespace MagicSpace.LS
     public class EnemyController : MonoBehaviour
     {
         [SerializeField]
-        private Animation enemyAnimator;
+        private Animator enemyAnimator;
 
         private Transform cachedTransform;
-        private List<string> runAnimations = new List<string>();
-        private List<string> hitAnimations = new List<string>();
         private int laneIndex = 0;
         private bool isDead = false;
 
@@ -27,7 +25,6 @@ namespace MagicSpace.LS
         public void Move(Vector3 newPosition)
         {
             cachedTransform.position = newPosition;
-            enemyAnimator.Play(runAnimations[Random.Range(0, runAnimations.Count)]);
         }
 
         public void Attack()
@@ -37,20 +34,12 @@ namespace MagicSpace.LS
 
         public void Hit(int animIndex)
         {
-            enemyAnimator.Play(hitAnimations[animIndex]);
             isDead = true;
         }
 
         private void Awake()
         {
             cachedTransform = this.transform;
-            runAnimations.Add("Run_1");
-            runAnimations.Add("Run_2");
-            hitAnimations.Add("Hit_1");
-            hitAnimations.Add("Hit_2");
-            hitAnimations.Add("Hit_3");
-
-            enemyAnimator.Play(runAnimations[Random.Range(0, runAnimations.Count)]);
         }
     }
 }
