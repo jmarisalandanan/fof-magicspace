03f9d2f07c27b39ea0c009c1d92e4a308a33e22e
jmarisalandanan <salandananjosemari@gmail.com>
2020-08-25 23:26:15 +0800

    Base animation work

--- FistOfFury/Assets/Scripts/Game/Enemy/EnemyController.cs
+++ FistOfFury/Assets/Scripts/Game/Enemy/EnemyController.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-using MagicSpace.Extensions;
-using UnityEngine.Events;
 
 namespace MagicSpace.LS
 {
     public class EnemyController : MonoBehaviour
     {
+        private static readonly int WalkHashValue = Animator.StringToHash("isWalking");
+        private static readonly int RunHashValue = Animator.StringToHash("isRunning");
+        private static readonly int AttackHashValue = Animator.StringToHash("isAttack");
+        private static readonly int HitHashValue = Animator.StringToHash("isHit");
+        
         [SerializeField]
         private Animator enemyAnimator;
 
-        public int LaneIndex { get; private set; } = 0;
-        public bool IsDead { get; private set; } = false;
+        public int LaneIndex { get; private set; }
+        public bool IsDead { get; private set; }
         public Transform CachedTransform { get; private set; }
 
         public void SetLaneIndex(int index)
@@ -21,22 +24,29 @@ namespace MagicSpace.LS
 
         public void Move(Vector3 newPosition)
         {
-            CachedTransform.position = newPosition;
+            enemyAnimator.SetBool(WalkHashValue, true);
         }
 
         public void Attack()
         {
             Debug.LogFormat("Enemy: {0} attacks", gameObject.name);
+            enemyAnimator.Play(AttackHashValue);
         }
 
         public void Hit(int animIndex)
         {
             IsDead = true;
+            enemyAnimator.Play(HitHashValue);
         }
 
         private void Awake()
         {
             CachedTransform = this.transform;
+        }
+
+        private void LateUpdate()
+        {
+           enemyAnimator.transform.LookAt(new Vector3(0,1,0));
         }
     }
 }
